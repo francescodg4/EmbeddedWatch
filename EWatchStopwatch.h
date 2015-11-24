85704120967ad29e56c2528d9684b2fe056c118b
Francesco de Gioia <francescodg4@gmail.com>
2015-11-24 15:27:49 +0100

    Added Reset functionality

--- EWatchStopwatch.h
+++ EWatchStopwatch.h
@@ -3,7 +3,7 @@
 
 #include "EWatchClock.h"
 
-enum EWatchStopwatchSignal {ST_CLOCK_TICK_SIG, ST_TOGGLE_SIG};
+enum EWatchStopwatchSignal {ST_CLOCK_TICK_SIG, ST_TOGGLE_SIG, ST_RESET_SIG};
 // enum EWatchStopwatchSignal {ST_CLOCK_TICK, ST_BUTTON_P, ST_BUTTON_M};
 enum EWatchStopwatchState {ST_RUNNING_STATE, ST_STOP_STATE};
 
