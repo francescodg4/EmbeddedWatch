144fe29e78ca9b09248e063365201bdb5cea5ade
Francesco de Gioia <francescodg4@gmail.com>
2015-11-24 01:04:58 +0100

    Signals as array of inputs

--- test/TestEWatch.c
+++ test/TestEWatch.c
@@ -1,6 +1,7 @@
 #include "unity.h"
 #include "../EWatch.h"
 
+const unsigned int CLOCK_UPDATE[2] = {CLOCK_TICK, 0};
 static EWatch watch;
 
 static int convertToTicks(int hours, int minutes, int seconds, int tenths)
@@ -34,7 +35,7 @@ void test_StartWatchWithInitialState(void)
 
 void test_SendClockTickSignalToWatch(void)
 {
-	EWatch_Dispatch(&watch, CLOCK_TICK);
+	EWatch_Dispatch(&watch, CLOCK_UPDATE);
 	TEST_ASSERT_EQUAL(1, EWatch_GetTenths(&watch));
 }
 
@@ -42,7 +43,7 @@ void test_TickOneSecondWhenTenthsArePassed(void)
 {
 	int i;
 	for (i = 0; i < 10; i++)
-		EWatch_Dispatch(&watch, CLOCK_TICK);
+		EWatch_Dispatch(&watch, CLOCK_UPDATE);
 
 	TEST_ASSERT_EQUAL(0, EWatch_GetTenths(&watch));
 	TEST_ASSERT_EQUAL(1, EWatch_GetSeconds(&watch));
@@ -52,7 +53,7 @@ void test_TickOneMinuteWhenSecondsArePassed(void)
 {
 	int i;
 	for (i = 0; i < 60 * 10; i++)
-		EWatch_Dispatch(&watch, CLOCK_TICK);
+		EWatch_Dispatch(&watch, CLOCK_UPDATE);
 
 	TEST_ASSERT_EQUAL(0, EWatch_GetSeconds(&watch));
 	TEST_ASSERT_EQUAL(1, EWatch_GetMinutes(&watch));
@@ -62,7 +63,7 @@ void test_TickOneHourWhenMinutesArePassed(void)
 {
 	int i;
 	for (i = 0; i < 60 * 60 * 10; i++)
-		EWatch_Dispatch(&watch, CLOCK_TICK);
+		EWatch_Dispatch(&watch, CLOCK_UPDATE);
 
 	TEST_ASSERT_EQUAL(0, EWatch_GetMinutes(&watch));
 	TEST_ASSERT_EQUAL(1, EWatch_GetHours(&watch));
@@ -71,10 +72,10 @@ void test_TickOneHourWhenMinutesArePassed(void)
 void test_ResetWhenADayIsPassed(void)
 {
 	int day = 24 * 60 * 60 * 10;
-	
+
 	int i;
 	for (i = 0; i < day; i++)
-		EWatch_Dispatch(&watch, CLOCK_TICK);
+		EWatch_Dispatch(&watch, CLOCK_UPDATE);
 	
 	TEST_ASSERT_EQUAL(0, EWatch_GetTenths(&watch));
 	TEST_ASSERT_EQUAL(0, EWatch_GetSeconds(&watch));
@@ -86,7 +87,7 @@ void test_CountShouldNotBeMoreThanDayLength(void)
 {
 	int i;
 	for (i = 0; i < TENTHS_IN_DAY; i++)
-		EWatch_Dispatch(&watch, CLOCK_TICK);
+		EWatch_Dispatch(&watch, CLOCK_UPDATE);
 
 	TEST_ASSERT_EQUAL(0, watch.counter);
 }
@@ -97,7 +98,7 @@ void test_CountTo3Hours20MinutesAnd30Seconds(void)
 
 	int i;
 	for (i = 0; i < ticks; i++)
-		EWatch_Dispatch(&watch, CLOCK_TICK);
+		EWatch_Dispatch(&watch, CLOCK_UPDATE);
 
 	TEST_ASSERT_EQUAL(3, EWatch_GetHours(&watch));
 	TEST_ASSERT_EQUAL(20, EWatch_GetMinutes(&watch));
@@ -111,7 +112,7 @@ void test_CountToDayLimit(void)
 	
 	int i;
 	for (i = 0; i < ticks; i++)
-		EWatch_Dispatch(&watch, CLOCK_TICK);
+		EWatch_Dispatch(&watch, CLOCK_UPDATE);
 
 	TEST_ASSERT_EQUAL(TENTHS_IN_DAY - 1, watch.counter);
 	TEST_ASSERT_EQUAL(23, EWatch_GetHours(&watch));
@@ -123,9 +124,10 @@ void test_CountToDayLimit(void)
 void test_SwitchToAlarmModeWhenSignalIsReceived(void)
 {
 	enum EWatchMode previous = EWatch_GetMode(&watch);
+	unsigned int sig1[] = {0, SET_ALARM_MODE};
 
-	EWatch_Dispatch(&watch, SET_ALARM_MODE);
-	EWatch_Dispatch(&watch, CLOCK_TICK);
+	EWatch_Dispatch(&watch, sig1);
+	EWatch_Dispatch(&watch, CLOCK_UPDATE);
 	
 	TEST_ASSERT_EQUAL_MESSAGE(CLOCK_MODE, previous, "Should be CLOCK_MODE");     
 	TEST_ASSERT_EQUAL(1, EWatch_GetTenths(&watch));
@@ -134,10 +136,11 @@ void test_SwitchToAlarmModeWhenSignalIsReceived(void)
 
 void test_SameSignalGoesIntoTwoHIerarchicalMachines(void)
 {
-  	EWatch_Dispatch(&watch, SET_CLOCK_MODE);
-	EWatch_Dispatch(&watch, CLOCK_TICK);
-	EWatch_Dispatch(&watch, SET_ALARM_MODE);
-	EWatch_Dispatch(&watch, CLOCK_TICK);
+	unsigned int sig1[] = {CLOCK_TICK, SET_CLOCK_MODE};
+	unsigned int sig2[] = {CLOCK_TICK, SET_ALARM_MODE};
+
+  	EWatch_Dispatch(&watch, sig1);
+	EWatch_Dispatch(&watch, sig2);
 
 	TEST_ASSERT_EQUAL(2, EWatch_GetTenths(&watch));
 	TEST_ASSERT_EQUAL(ALARM_MODE, EWatch_GetMode(&watch));
