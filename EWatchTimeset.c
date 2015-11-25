f4b0ffd7e87d310ecf4d1b9e5a655ff5e7f64791
Francesco de Gioia <francescodg4@gmail.com>
2015-11-25 16:19:54 +0100

    Passed add hour, add minutes, add more hours and add more minutes

--- EWatchTimeset.c
+++ EWatchTimeset.c
@@ -1,18 +1,53 @@
 #include "EWatchTimeset.h"
 
+static void transition(EWatchTimeset *this, enum EWatchTimesetState state)
+{
+	this->state = state;
+}
+
 void EWatchTimeset_Init(EWatchTimeset *this)
 {
-	EWatchClock_Init(&this->internal);
+	transition(this, TS_SET_HOURS_STATE);
+	ClockCounter_Init(&this->internal);	
 }
 
 void EWatchTimeset_Dispatch(EWatchTimeset *this, enum EWatchTimesetSignal sig)
 {
-	
+	switch (this->state) {
+	case TS_SET_HOURS_STATE:
+		switch (sig) {
+
+		case TS_INC_HOURS_SIG:
+			ClockCounter_Increment(&this->internal, TENTHS_IN_HOUR);
+			break;
+			
+		case TS_SET_MINUTES_MODE_SIG:
+			transition(&this->internal, TS_SET_MINUTES_STATE);
+			break;
+
+		default:
+			break;
+		}
+
+		break;
+
+	case TS_SET_MINUTES_STATE:
+		switch (sig) {
+		case TS_INC_MINUTES_SIG:
+			ClockCounter_Increment(&this->internal, TENTHS_IN_MINUTE);
+		default:
+			break;
+		}
+		break;
+
+	default:
+		break;
+	}
 }
 
 int EWatchTimeset_GetHours(EWatchTimeset *this)
 {
-	return 0;
+	return ClockCounter_GetHours(&this->internal);
 }
 
 int EWatchTimeset_GetMinutes(EWatchTimeset *this)
