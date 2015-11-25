852963e257b842caa9cd5e845c04f30a14d2970f
francescodg4 <francescodg4@gmail.com>
2015-11-25 12:20:53 +0100

    Added timeset functionality

---
+++ EWatchTimeset.h
@@ -0,0 +1,25 @@
+#ifndef EWATCHTIMESET_H
+#define EWATCHTIMESET_H
+
+#include "EWatchClock.h"
+
+enum EWatchTimesetSignal {
+	TS_INC_HOURS_SIG,
+	TS_DEC_HOURS_SIG,
+	TS_INC_MINUTES_SIG,
+	TS_DEC_MINUTES_SIG};
+
+enum EWatchTimesetState {TS_SET_HOURS_STATE, TS_SET_MINUTES_STATE};
+
+typedef struct {
+	EWatchClock internal;
+} EWatchTimeset;
+
+void EWatchTimeset_Init(EWatchTimeset *this);
+void EWatchTimeset_Dispatch(EWatchTimeset *this, enum EWatchTimesetSignal sig);
+int EWatchTimeset_GetHours(EWatchTimeset *this);
+int EWatchTimeset_GetMinutes(EWatchTimeset *this);
+int EWatchTimeset_GetSeconds(EWatchTimeset *this);
+int EWatchTimeset_GetTenths(EWatchTimeset *this);
+
+#endif /* EWATCHTIMESET_H */
