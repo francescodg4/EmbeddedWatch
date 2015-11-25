2e141327de8f1667f3e1cd19140954918a7dfdec
Francesco de Gioia <francescodg4@gmail.com>
2015-11-25 16:19:54 +0100

    Passed add hour, add minutes, add more hours and add more minutes

--- EWatchTimeset.h
+++ EWatchTimeset.h
@@ -2,17 +2,22 @@
 #define EWATCHTIMESET_H
 
 #include "EWatchClock.h"
+#include "ClockCounter.h"
 
 enum EWatchTimesetSignal {
 	TS_INC_HOURS_SIG,
 	TS_DEC_HOURS_SIG,
 	TS_INC_MINUTES_SIG,
-	TS_DEC_MINUTES_SIG};
+	TS_DEC_MINUTES_SIG,
+	TS_SET_HOURS_MODE_SIG,
+	TS_SET_MINUTES_MODE_SIG
+};
 
 enum EWatchTimesetState {TS_SET_HOURS_STATE, TS_SET_MINUTES_STATE};
 
 typedef struct {
-	EWatchClock internal;
+	ClockCounter internal;
+	enum EWatchTimesetState state;
 } EWatchTimeset;
 
 void EWatchTimeset_Init(EWatchTimeset *this);
--- test/TestEWatchTimeset.c
+++ test/TestEWatchTimeset.c
@@ -2,30 +2,56 @@
 #include "../EWatchTimeset.h"
 #include "utility.h"
 
+static EWatchTimeset w;
+
+void setUp(void)
+{
+	EWatchTimeset_Init(&w);
+}
+
 void test_InitilizedAtTime0(void)
 {
 	EWatchTimeset w;
 	EWatchTimeset_Init(&w);
 
 	checkTime(0, 0, 0, 0, &w.internal);
+	TEST_ASSERT_EQUAL_MESSAGE(TS_SET_HOURS_STATE, w.state, "Expected TS_SET_HOURS_STATE");
 }
 
 void test_AddHour(void)
 {
-	TEST_IGNORE();
+	EWatchTimeset_Dispatch(&w, TS_INC_HOURS_SIG);
+	
+	checkTime(1, 0, 0, 0, &w.internal);
 }
 
 void test_AddMinute(void)
 {
-	TEST_IGNORE();
+	EWatchTimeset_Dispatch(&w, TS_SET_MINUTES_MODE_SIG);
+	EWatchTimeset_Dispatch(&w, TS_INC_MINUTES_SIG);
+	
+	checkTime(0, 1, 0, 0, &w.internal);
+}
+
+
+void test_AddMoreHours(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+		EWatchTimeset_Dispatch(&w, TS_INC_HOURS_SIG);
+
+	checkTime(5, 0, 0, 0, &w.internal);
 }
 
 void test_AddMoreMinutes(void)
 {
-	TEST_IGNORE();
+	EWatchTimeset_Dispatch(&w, TS_SET_MINUTES_MODE_SIG);
+	
+	int i;
+	for (i = 0; i < 50; i++)
+		EWatchTimeset_Dispatch(&w, TS_INC_MINUTES_SIG);
+
+	checkTime(0, 50, 0, 0, &w.internal);	
 }
 
-void test_AddMoreHours(void)
-{
-	TEST_IGNORE();
-}
