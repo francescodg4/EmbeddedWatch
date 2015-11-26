8088b19ddd93bd06daacb9c8c022e3449128c3fe
francescodg4 <francescodg4@gmail.com>
2015-11-26 15:54:41 +0100

    Passed Alarm will expire if external time changes

--- EWatchAlarm.c
+++ EWatchAlarm.c
@@ -5,12 +5,31 @@ static void transition(EWatchAlarm *this, enum EWatchAlarmState state);
 void EWatchAlarm_Init(EWatchAlarm *this)
 {
 	unsigned int defaultTime = convertToTenths(12, 0, 0, 0);
+
 	ClockCounter_Init(&this->alarmCounter);
 	ClockCounter_Set(&this->alarmCounter, defaultTime);
+	ClockCounter_Set(&this->finalCounter, defaultTime);
 
 	this->alarmState = ALARM_OFF;
-	ClockCounter_Set(&this->finalCounter, defaultTime);
+	
+	transition(this, AL_ALARM_OFF_STATE);
 }
+
+void EWatchAlarm_Init2(EWatchAlarm *this, ClockCounter *external)
+{
+	unsigned int defaultTime = convertToTenths(12, 0, 0, 0);
+
+// 	ClockCounter_Init(&this->alarmCounter);
+//	ClockCounter_Set(&this->alarmCounter, defaultTime);
+	ClockCounter_Set(&this->finalCounter, defaultTime);
+	// ClockCounter_Set(&this->alarmCounter, defaultTime);
+	this->external = external;	
+
+	this->alarmState = ALARM_OFF;
+
+	transition(this, AL_ALARM_OFF_STATE);
+}
+
 
 void EWatchAlarm_Dispatch(EWatchAlarm *this, enum EWatchAlarmSignal sig)
 {
@@ -30,7 +49,7 @@ void EWatchAlarm_Dispatch(EWatchAlarm *this, enum EWatchAlarmSignal sig)
 	case AL_ALARM_ON_STATE:
 		switch (sig) {
 		case AL_CLOCK_TICK_SIG:
-			if (ClockCounter_GetCount(&this->alarmCounter) == 
+			if (ClockCounter_GetCount(this->external) == 
 			    ClockCounter_GetCount(&this->finalCounter)) {
 				transition(this, AL_ALARM_EXPIRED_STATE);
 				this->alarmState = ALARM_EXPIRED;
@@ -53,12 +72,12 @@ void EWatchAlarm_Dispatch(EWatchAlarm *this, enum EWatchAlarmSignal sig)
 
 int EWatchAlarm_GetHours(EWatchAlarm *this)
 {
-	return ClockCounter_GetHours(&this->alarmCounter);
+	return ClockCounter_GetHours(&this->finalCounter);
 }
 
 int EWatchAlarm_GetMinutes(EWatchAlarm *this)
 {
-	return ClockCounter_GetMinutes(&this->alarmCounter);
+	return ClockCounter_GetMinutes(&this->finalCounter);
 }
 
 enum AlarmState EWatchAlarm_GetAlarmState(EWatchAlarm *this)
--- test/TestEWatchAlarm.c
+++ test/TestEWatchAlarm.c
@@ -2,16 +2,19 @@
 #include "../EWatchAlarm.h"
 
 static EWatchAlarm alarm;
+static ClockCounter external;
 
 void setUp(void)
-{
-	EWatchAlarm_Init(&alarm);	
+{	
+	ClockCounter_Init(&external);
+	EWatchAlarm_Init2(&alarm, &external);	
 }
 
 void test_InitializeWithDefaultAlarmAt12(void)
 {
 	EWatchAlarm alarm;
-	EWatchAlarm_Init(&alarm);
+
+	EWatchAlarm_Init2(&alarm, &external);
 	
 	TEST_ASSERT_EQUAL(12, EWatchAlarm_GetHours(&alarm));
 	TEST_ASSERT_EQUAL(0, EWatchAlarm_GetMinutes(&alarm));
@@ -27,11 +30,30 @@ void test_SetAlarmWaitForTicksAndExpire(void)
 	int ticks = convertToTicks(12, 0, 0, 0);
 	int i;
 	
-	for (i = 0; i < ticks; i++)
+	for (i = 0; i < ticks; i++) {
+		ClockCounter_Tick(&external);
 		EWatchAlarm_Dispatch(&alarm, AL_CLOCK_TICK_SIG);
+	}
 	
 	enum AlarmState currentState = EWatchAlarm_GetAlarmState(&alarm);
 
 	TEST_ASSERT_EQUAL_MESSAGE(ALARM_ON, prevState, "Expected ALARM_OFF");
 	TEST_ASSERT_EQUAL_MESSAGE(ALARM_EXPIRED, currentState, "Expected ALARM_EXPIRED");
+}
+
+void test_AlarmWillExpireIfExternalTimeIsChanged(void)
+{
+	ClockCounter external;
+	EWatchAlarm_Init2(&alarm, &external);
+	
+	ClockCounter_Set(&external, convertToTenths(11, 59, 59, 9));
+	EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG);
+
+	TEST_ASSERT_EQUAL_MESSAGE(ALARM_ON, EWatchAlarm_GetAlarmState(&alarm), "Expected ALARM_ON");
+	ClockCounter_Tick(&external);
+	EWatchAlarm_Dispatch(&alarm, AL_CLOCK_TICK_SIG);
+	
+	enum AlarmState alarmState = EWatchAlarm_GetAlarmState(&alarm);
+
+	TEST_ASSERT_EQUAL_MESSAGE(ALARM_EXPIRED, alarmState, "Expected ALARM_EXPIRED");
 }
