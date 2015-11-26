3ed93a7e1657d5c1105d4622b9a707d04ec588ca
Francesco de Gioia <francescodg4@gmail.com>
2015-11-26 21:44:53 +0100

    Passed entering set alarm mode and set hours and seconds

--- EWatchAlarm.c
+++ EWatchAlarm.c
@@ -1,15 +1,15 @@
 #include "EWatchAlarm.h"
 #include "EWatchTimeset.h"
 
-static EWatchTimeset timeset;
+// static EWatchTimeset timeset;
 static void transition(EWatchAlarm *this, enum EWatchAlarmState state);
-static void setupAlarm(EWatchAlarm *this);
 
 void EWatchAlarm_Init(EWatchAlarm *this, ClockCounter *external)
 {
 	unsigned int defaultTime = convertToTenths(12, 0, 0, 0);
-	ClockCounter_Set(&this->expirationTime, defaultTime);
-
+	// ClockCounter_Set(&this->expirationTime, defaultTime);
+	EWatchTimeset_Init(&this->expirationTime);
+	EWatchTimeset_Set(&this->expirationTime, defaultTime);
 	this->external = external;	
 	this->alarmState = ALARM_OFF;
 
@@ -18,7 +18,7 @@ void EWatchAlarm_Init(EWatchAlarm *this, ClockCounter *external)
 
 void EWatchAlarm_Dispatch(EWatchAlarm *this, enum EWatchAlarmSignal sig)
 {
-	unsigned int prevExpirationTime;
+	// unsigned int prevExpirationTime;
 
 	switch (this->state) {
 	case AL_ALARM_OFF_STATE:
@@ -26,30 +26,31 @@ void EWatchAlarm_Dispatch(EWatchAlarm *this, enum EWatchAlarmSignal sig)
 		case AL_ALARM_SET_SIG:
 			transition(this, AL_SET_ALARM_HOURS_STATE);
 
-			prevExpirationTime = ClockCounter_GetCount(&this->expirationTime);
-			EWatchTimeset_Init(&timeset);
-			EWatchTimeset_Set(&timeset, prevExpirationTime);
+			// prevExpirationTime = ClockCounter_GetCount(&this->expirationTime);
+			// EWatchTimeset_Init(&timeset);
+			// EWatchTimeset_Set(&this->expirationTime, prevExpirationTime);
 
 			break;
 
 		default:
 			break;
 		}
+
 		break;
 
 	case AL_SET_ALARM_HOURS_STATE:
 		switch (sig) {
 
 		case AL_INC_SIG:
-			EWatchTimeset_Dispatch(&timeset, TS_INC_SIG);
+			EWatchTimeset_Dispatch(&this->expirationTime, TS_INC_SIG);
 			break;
 
 		case AL_DEC_SIG:
-			EWatchTimeset_Dispatch(&timeset, TS_DEC_SIG);
+			EWatchTimeset_Dispatch(&this->expirationTime, TS_DEC_SIG);
 			break;
 
 		case AL_ALARM_SET_SIG:
-			EWatchTimeset_Dispatch(&timeset, TS_TOGGLE_MODE_SIG);
+			EWatchTimeset_Dispatch(&this->expirationTime, TS_TOGGLE_MODE_SIG);
 			transition(this, AL_SET_ALARM_MINUTES_STATE);
 			break;
 
@@ -57,20 +58,21 @@ void EWatchAlarm_Dispatch(EWatchAlarm *this, enum EWatchAlarmSignal sig)
 			break;
 		}
 
+		break;
+
 	case AL_SET_ALARM_MINUTES_STATE:
 		switch (sig) {
 
 		case AL_INC_SIG:
-			EWatchTimeset_Dispatch(&timeset, TS_INC_SIG);
+			EWatchTimeset_Dispatch(&this->expirationTime, TS_INC_SIG);
 			break;
 
 		case AL_DEC_SIG:
-			EWatchTimeset_Dispatch(&timeset, TS_DEC_SIG);
+			EWatchTimeset_Dispatch(&this->expirationTime, TS_DEC_SIG);
 			break;
 
 		case AL_ALARM_SET_SIG:
 			transition(this, AL_ALARM_ON_STATE);
-			setupAlarm(this);
 			this->alarmState = ALARM_ON;
 			break;
 
@@ -83,11 +85,13 @@ void EWatchAlarm_Dispatch(EWatchAlarm *this, enum EWatchAlarmSignal sig)
 	case AL_ALARM_ON_STATE:
 		switch (sig) {
 		case AL_CLOCK_TICK_SIG:
+			/*
 			if (ClockCounter_GetCount(this->external) == 
 			    ClockCounter_GetCount(&this->expirationTime)) {
 				transition(this, AL_ALARM_EXPIRED_STATE);
 				this->alarmState = ALARM_EXPIRED;
 			}
+			*/
 			break;
 		
 		default:
@@ -106,12 +110,12 @@ void EWatchAlarm_Dispatch(EWatchAlarm *this, enum EWatchAlarmSignal sig)
 
 int EWatchAlarm_GetHours(EWatchAlarm *this)
 {
-	return ClockCounter_GetHours(&this->expirationTime);
+	return EWatchTimeset_GetHours(&this->expirationTime);
 }
 
 int EWatchAlarm_GetMinutes(EWatchAlarm *this)
 {
-	return ClockCounter_GetMinutes(&this->expirationTime);
+	return EWatchTimeset_GetMinutes(&this->expirationTime);
 }
 
 enum AlarmState EWatchAlarm_GetAlarmState(EWatchAlarm *this)
@@ -122,10 +126,4 @@ enum AlarmState EWatchAlarm_GetAlarmState(EWatchAlarm *this)
 static void transition(EWatchAlarm *this, enum EWatchAlarmState state)
 {
 	this->state = state;
-}
-
-static void setupAlarm(EWatchAlarm *this)
-{
-	unsigned int newAlarm = EWatchTimeset_GetCount(&timeset);
-	ClockCounter_Set(&this->expirationTime, newAlarm);
 }
