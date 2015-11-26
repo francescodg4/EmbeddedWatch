e6b85aae7c417685851de2491f3400faaeb517bd
francescodg4 <francescodg4@gmail.com>
2015-11-26 14:55:31 +0100

    Passed initialization with alarm state

--- EWatchAlarm.h
+++ EWatchAlarm.h
@@ -3,12 +3,16 @@
 
 #include "ClockCounter.h"
 
+enum AlarmState {ALARM_OFF, ALARM_ON, ALARM_EXPIRED};
+
 typedef struct {
 	ClockCounter alarmCounter;
+	enum AlarmState alarmState;
 } EWatchAlarm;
 
 void EWatchAlarm_Init(EWatchAlarm *this);
 
+enum AlarmState EWatchAlarm_GetAlarmState(EWatchAlarm *this);
 int EWatchAlarm_GetHours(EWatchAlarm *this);
 int EWatchAlarm_GetMinutes(EWatchAlarm *this);
 
