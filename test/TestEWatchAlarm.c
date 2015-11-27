ccc737f4d89cae9cd1ad0ba2bf01c0c1b5b3b87d
Francesco de Gioia <francescodg4@gmail.com>
2015-11-27 12:02:39 +0100

    Tick and expire refactored

--- test/TestEWatchAlarm.c
+++ test/TestEWatchAlarm.c
@@ -43,49 +43,74 @@ void test_SwicthAlarmStateOnWhenSignalReceived(void)
 	TEST_ASSERT_EQUAL(ALARM_ON, EWatchAlarm_GetAlarmState(&alarm));       
 }
 
-/* void test_SetAlarmWaitForTicksAndExpire(void) */
-/* { */
-/* 	EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG); */
+static void setAlarmTo(EWatchAlarm *a, int hours, int minutes)
+{	
+	EWatchTimeset_Set(&a->expirationTime, 0);
 
-/* 	enum AlarmState prevState = EWatchAlarm_GetAlarmState(&alarm);	 */
+	int i;
+	EWatchAlarm_Dispatch(a, AL_ALARM_SET_SIG); // Set hours
 
-/* 	waitFor(12, 0, 0, 0); */
+	for (i = 0; i < hours; i++)
+		EWatchAlarm_Dispatch(a, AL_INC_SIG);
+	
+	EWatchAlarm_Dispatch(a, AL_ALARM_SET_SIG); // Set minutes
 
-/* 	enum AlarmState currentState = EWatchAlarm_GetAlarmState(&alarm); */
+	for (i = 0; i < minutes; i++)
+		EWatchAlarm_Dispatch(a, AL_INC_SIG);
+	
+	EWatchAlarm_Dispatch(a, AL_ALARM_SET_SIG); // Set alarm
+}
 
-/* 	TEST_ASSERT_EQUAL_MESSAGE(ALARM_ON, prevState, "Expected ALARM_OFF"); */
-/* 	TEST_ASSERT_EQUAL_MESSAGE(ALARM_EXPIRED, currentState, "Expected ALARM_EXPIRED"); */
-/* } */
+void test_SetAlarmWaitForTicksAndExpire(void)
+{
+	EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG); // Switch to alarm
+	/* EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG); // Set hours */
+	/* EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG); // Set minutes */
+	/* EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG); // Set alarm */
 
-/* void test_AlarmWillExpireIfExternalTimeIsChanged(void) */
-/* { */
-/* 	ClockCounter_Set(&external, convertToTenths(11, 59, 59, 9)); */
+	setAlarmTo(&alarm, 12, 0);
 
-/* 	EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG); */
+	enum AlarmState prevState = EWatchAlarm_GetAlarmState(&alarm);
+
+	waitFor(12, 0, 0, 0);
+
+	enum AlarmState currentState = EWatchAlarm_GetAlarmState(&alarm);
+
+	TEST_ASSERT_EQUAL_MESSAGE(ALARM_ON, prevState, "Expected ALARM_OFF");
+	TEST_ASSERT_EQUAL_MESSAGE(ALARM_EXPIRED, currentState, "Expected ALARM_EXPIRED");
+}
+
+void test_AlarmWillExpireIfExternalTimeIsChanged(void)
+{
+	ClockCounter_Set(&external, convertToTenths(11, 59, 59, 9));
+
+	EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG); // Switch to alarm
+   
+	setAlarmTo(&alarm, 12, 0);
+
+	ClockCounter_Tick(&external);
+	EWatchAlarm_Dispatch(&alarm, AL_CLOCK_TICK_SIG);
+	
+	enum AlarmState alarmState = EWatchAlarm_GetAlarmState(&alarm);
+
+	TEST_ASSERT_EQUAL_MESSAGE(ALARM_EXPIRED, alarmState, "Expected ALARM_EXPIRED");
+}
+
+void test_AlarmIsNotYetExpired(void)
+{
+	ClockCounter_Set(&external, convertToTenths(11, 59, 59, 8));
+	
+	EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG);
        
-/* 	ClockCounter_Tick(&external); */
-/* 	EWatchAlarm_Dispatch(&alarm, AL_CLOCK_TICK_SIG); */
+	ClockCounter_Tick(&external);
+	EWatchAlarm_Dispatch(&alarm, AL_CLOCK_TICK_SIG);
 	
-/* 	enum AlarmState alarmState = EWatchAlarm_GetAlarmState(&alarm); */
-
-/* 	TEST_ASSERT_EQUAL_MESSAGE(ALARM_EXPIRED, alarmState, "Expected ALARM_EXPIRED"); */
-/* } */
-
-/* void test_AlarmIsNotYetExpired(void) */
-/* { */
-/* 	ClockCounter_Set(&external, convertToTenths(11, 59, 59, 8)); */
+	enum AlarmState alarmState = EWatchAlarm_GetAlarmState(&alarm);
 	
-/* 	EWatchAlarm_Dispatch(&alarm, AL_ALARM_SET_SIG); */
-       
-/* 	ClockCounter_Tick(&external); */
-/* 	EWatchAlarm_Dispatch(&alarm, AL_CLOCK_TICK_SIG); */
-	
-/* 	enum AlarmState alarmState = EWatchAlarm_GetAlarmState(&alarm); */
-	
-/* 	TEST_ASSERT_EQUAL_MESSAGE(ALARM_ON, alarmState, "Expected ALARM_ON"); */
-/* } */
+	TEST_ASSERT_EQUAL_MESSAGE(ALARM_ON, alarmState, "Expected ALARM_ON");
+}
 
 /* void test_SetAlarmTo11am(void) */
 /* { */
-/* 	EWatchAlarm_Dispatch(&alarm,  */
-//}
+/* 	EWatchAlarm_Dispatch(&alarm, */
+/* } */
